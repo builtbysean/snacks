47510ce4368fbfa3c78d5542952c0caab7485637
builtbysean <seanoffin2003@gmail.com>
2025-07-08 19:06:50 -0400

    Initial Commit

---
+++ Snacks/SnacksApp.swift
@@ -0,0 +1,17 @@
+//
+//  SnacksApp.swift
+//  Snacks
+//
+//  Created by Sean Coffin on 7/8/25.
+//
+
+import SwiftUI
+
+@main
+struct SnacksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
